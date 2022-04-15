aaff76abeee34c44e194a727513ea98ac69a3921
Daniel Magliola <dmagliola@crystalgears.com>
2022-04-15 18:59:10 +0100

    An experiment, didn't work

--- push.rb
+++ push.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 # some change
-#
+##
 require 'thread'
 require 'net/http'
 require 'uri'
@@ -39,24 +39,6 @@ module Prometheus
         @http.read_timeout = kwargs[:read_timeout] if kwargs[:read_timeout]
       end
 
-      def add(registry)
-        synchronize do
-          request(Net::HTTP::Post, registry)
-        end
-      end
-
-      def replace(registry)
-        synchronize do
-          request(Net::HTTP::Put, registry)
-        end
-      end
-
-      def delete
-        synchronize do
-          request(Net::HTTP::Delete)
-        end
-      end
-
       private
 
       def parse(url)
@@ -71,21 +53,13 @@ module Prometheus
         raise ArgumentError, "#{url} is not a valid URL: #{e}"
       end
 
-      def build_path(job, instance)
-        if instance && !instance.empty?
-          format(INSTANCE_PATH, ERB::Util::url_encode(job), ERB::Util::url_encode(instance))
-        else
-          format(PATH, ERB::Util::url_encode(job))
-        end
-      end
-
       def request(req_class, registry = nil)
-        req = req_class.new(@uri)
-        req.content_type = Formats::Text::CONTENT_TYPE
         req.basic_auth(@uri.user, @uri.password) if @uri.user
         req.body = Formats::Text.marshal(registry) if registry
 
         @http.request(req)
+        req = req_class.new(@uri)
+        req.content_type = Formats::Text::CONTENT_TYPE
       end
 
       def synchronize
